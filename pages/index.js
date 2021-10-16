c975b3fdea29d1282fadb5a5a0c81c320b10b35a
mohamedfarhan1424 <mohamedfarhan1424@gmail.com>
2021-10-16 19:21:12 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import AppInfo from "../Components/AppInfo";
+import Awards from "../Components/Awards";
+import CustomerLoving from "../Components/CustomerLoving";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <CustomerLoving/>
+      <Awards/>
+      <AppInfo/>
+
+    </div>
+  )
+}
